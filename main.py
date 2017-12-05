fd3988f005cee78761011d21eead4f4077133cae
Paul Brown <pbrownpaul@gmail.com>
2017-12-05 21:12:51 +0000

    Describe execution

--- main.py
+++ main.py
@@ -9,23 +9,32 @@ from christmas_tree_twitter_auth import consumer_key, consumer_secret, access_to
 
 
 class ChristmasTreeListener(StreamListener):
+    """Stream Listener and Tree LED controller"""
+    
     tree = LEDBoard(*range(2,28), pwm=True)
+    # Need 26 keywords for the LEDs, the first is the star
     keywords = ["star", 
                 "snow", "christmas", "santa", "merry", "xmas",
                 "present", "gift", "card", "mince", "turkey",
                 "joy", "tree", "light", "decoration", "carol",
                 "cake", "wrap", "elves", "jingle", "reindeer",
                 "festive", "holiday", "sleigh", "nativity", "peace"]
+    # Associate each word with an LED via a dictionary for lookups
     lookup = {word: led for word, led in zip(keywords, tree.leds)}
+    # Keep track of how many times a word has been found via a counter dictionary
     word_counter = {word: 0 for word in keywords}
 
     def on_data(self, data):
+        """When new data is received from the Twitter stream"""
+        # Parse from JSON string
         parsed_data = json.loads(data)
-        if "text" in parsed_data:
+        # May not contain text, may be limit if the result is < 1% of tweets batch.
+        if "text" in parsed_data:  # checks parsed_data's keys for "text"
+            # Search for the words, if found, flip the led
             for word, led in self.lookup.items():
-                if word in parsed_data["text"]:
+                if word in parsed_data["text"]: 
                     self.word_counter[word] += 1
-                    led.toggle()
+                    led.toggle() # if on, turn off, else turn on.
         return True
 
     def on_error(self, status):
@@ -33,12 +42,17 @@ class ChristmasTreeListener(StreamListener):
 
 
 if __name__ == "__main__":
+    # Authenticate with Twitter
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
+    # Setup stream
     listener = ChristmasTreeListener()
     stream = Stream(auth, listener)
 
+    # Run filter, passing data to listener.
+    # In try/except block so a keyboard interrupt will 
+    # output the word counter, sorted by value
     try:
         stream.filter(track=listener.keywords)
     except KeyboardInterrupt:
